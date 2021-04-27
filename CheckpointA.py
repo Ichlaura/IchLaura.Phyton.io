886f4c91d1907235b55e00f782d5199ea730ad78
Ichlaura <laupao41@gmail.com>
2021-04-26 21:36:18 -0500

    initial commit

---
+++ CheckpointA.py
@@ -0,0 +1,11 @@
+
+# Laura Nuñez - CS 241 - BYU -Idaho 
+# Week 1 
+# Prepare: Checkpoint A 
+# Task: Write and submit a simple program in Python
+# Purpose:Learn the basics of Python
+
+
+print("Hello World! , I am Laura")
+
+
