92271491b8b00a8658845e3d31bcf9535bf892a7
Ichlaura <laupao41@gmail.com>
2021-04-28 20:25:54 -0500

    initial comit

---
+++ W2-CheckpointB.py
@@ -0,0 +1,9 @@
+
+
+# Laura Nuñez - CS 241 - BYU -Idaho 
+# Week 2 
+# W02 Prepare: Checkpoint B
+# Task: Demonstrate file reading
+# Purpose:Correctly use files and functions to solve problems.
+
+
